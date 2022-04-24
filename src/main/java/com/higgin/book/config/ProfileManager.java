013b4cd6449606a79ee8bcdb604e89cdd1a109b0
kunip004 <prajesh.x.kunimal.-nd@disney.com>
2022-04-24 13:24:49 -0400

    feature/PG-Container MetaDetails- implementation.

--- src/main/java/com/higgin/book/config/ProfileManager.java
+++ src/main/java/com/higgin/book/config/ProfileManager.java
@@ -1,11 +1,15 @@
 package com.higgin.book.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProfileManager {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ProfileManager.class);
 
     @Autowired
     private Environment environment;
@@ -19,11 +23,11 @@ public class ProfileManager {
     public void getActiveProfiles() {
         System.out.println("Checking profile details ");
         for (String profileName : environment.getActiveProfiles()) {
-            System.out.println("Currently active profile - " + profileName);
+            LOGGER.info("Currently active profile - " + profileName.toString());
         }
 
         for (String properties : environment.getDefaultProfiles()) {
-            System.out.println("Currently active properties - " + properties);
+            LOGGER.info("Currently active properties - " + properties.toString());
         }
     }
 }
